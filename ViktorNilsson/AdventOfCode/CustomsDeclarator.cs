d4755a3f16e1113206e37191d513dc59058d0d90
Viktor Nilsson <viktor_741@hotmail.com>
2020-12-06 19:29:42 +0100

    Solve Day 6.

---
+++ ViktorNilsson/AdventOfCode/CustomsDeclarator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TestAdventOfCode
+{
+    public class CustomsDeclarator
+    {
+
+        public static string[] GetInputData(string filePath)
+        {
+            string[] groupAnswers = InputDataHandler.ReadFileAsArray(filePath, "\r\n\r\n");
+
+            return groupAnswers;
+        }
+
+        public static int CountAnswers(string groupAnswer)
+        {
+            string cleanedAnswer = Regex.Replace(groupAnswer, @"\s+", string.Empty);
+            string uniqueAnswers = new String(cleanedAnswer.Distinct().ToArray());
+            int uniquePositiveAnswers = uniqueAnswers.Length;
+            return uniquePositiveAnswers;
+        }
+
+        public static int SumAnswers(string[] groupAnswerArray)
+        {
+            int sum = 0;
+            foreach (var groupAnswer in groupAnswerArray)
+            {
+                sum += CountAnswers(groupAnswer);
+            }
+
+            return sum;
+        }
+
+        public static int CountConsensusAnswers(string groupAnswer)
+        {
+            string[] answerArray = groupAnswer.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+
+            if (answerArray.Length == 1)
+            {
+                return answerArray[0].Length;
+            }
+
+            // Start with person one and check one by one who also asnwered yes
+            var answerConsensus = answerArray[0].ToArray().Intersect(answerArray[1].ToArray());
+            for (int iAnswer = 2; iAnswer < answerArray.Length; iAnswer++)
+            {
+                answerConsensus = answerConsensus.Intersect(answerArray[iAnswer].ToArray());
+            }
+
+            return answerConsensus.ToArray().Length;
+        }
+
+        public static int SumConsensusAnswers(string[] groupAnswerArray)
+        {
+            int sum = 0;
+            foreach (var groupAnswer in groupAnswerArray)
+            {
+                sum += CountConsensusAnswers(groupAnswer);
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ ViktorNilsson/TestAdventOfCode/TestCustomsDeclarator.cs
@@ -0,0 +1,84 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace TestAdventOfCode
+{
+    public class TestCustomsDeclarator
+    {
+        private const string FilePathInputData = "../../../../AdventOfCode/InputData/DaySixInput.txt";
+        private const string FilePathTestData = "../../../TestData/DaySixTestData.txt";
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestGetInputData()
+        {
+            string[] groupAnswers = CustomsDeclarator.GetInputData(FilePathTestData);
+            Assert.AreEqual(5, groupAnswers.GetLength(0));
+            //Assert.AreEqual("abc", groupAnswers[0]);
+            //Assert.AreEqual("abc", groupAnswers[1]);
+        }
+
+        [TestCase(0, 3)]
+        [TestCase(1, 3)]
+        [TestCase(2, 3)]
+        [TestCase(3, 1)]
+        [TestCase(4, 1)]
+        public void TestCountAnswers(int iTestData, int expectedAnswers)
+        {
+            string[] rawData = CustomsDeclarator.GetInputData(FilePathTestData);
+            int positiveAnswers = CustomsDeclarator.CountAnswers(rawData[iTestData]);
+            Assert.AreEqual(expectedAnswers, positiveAnswers);
+        }
+
+        [Test]
+        public void TestSumAnswers()
+        {
+            string[] rawData = CustomsDeclarator.GetInputData(FilePathTestData);
+            int sumOfAnswers = CustomsDeclarator.SumAnswers(rawData);
+            Assert.AreEqual(11, sumOfAnswers);
+        }
+
+        [Test]
+        public void FindAnserDaySixPuzzleOne()
+        {
+            string[] rawData = CustomsDeclarator.GetInputData(FilePathInputData);
+            int sumOfAnswers = CustomsDeclarator.SumAnswers(rawData);
+            Console.WriteLine("Number of valid passports: " + sumOfAnswers);
+            Assert.AreEqual(6630, sumOfAnswers);
+        }
+
+        [TestCase(0, 3)]
+        [TestCase(1, 0)]
+        [TestCase(2, 1)]
+        [TestCase(3, 1)]
+        [TestCase(4, 1)]
+        public void TestCountConsensusAnswers(int iTestData, int expectedAnswers)
+        {
+            string[] rawData = CustomsDeclarator.GetInputData(FilePathTestData);
+            int positiveAnswers = CustomsDeclarator.CountConsensusAnswers(rawData[iTestData]);
+            Assert.AreEqual(expectedAnswers, positiveAnswers);
+        }
+
+        [Test]
+        public void TestSumConsensusAnswers()
+        {
+            string[] rawData = CustomsDeclarator.GetInputData(FilePathTestData);
+            int sumOfAnswers = CustomsDeclarator.SumConsensusAnswers(rawData);
+            Assert.AreEqual(6, sumOfAnswers);
+        }
+
+        [Test]
+        public void FindAnserDaySixPuzzleTwo()
+        {
+            string[] rawData = CustomsDeclarator.GetInputData(FilePathInputData);
+            int sumOfAnswers = CustomsDeclarator.SumConsensusAnswers(rawData);
+            Console.WriteLine("Number of valid passports: " + sumOfAnswers);
+            Assert.AreEqual(3437, sumOfAnswers);
+        }
+    }
+}
