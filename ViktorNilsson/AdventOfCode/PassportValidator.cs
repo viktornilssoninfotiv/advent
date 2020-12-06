769ae283377ded03410f5b100ffbb4172ef7a796
Viktor Nilsson <viktor_741@hotmail.com>
2020-12-06 10:25:51 +0100

    Pass all example passports for day 4

--- ViktorNilsson/AdventOfCode/PassportValidator.cs
+++ ViktorNilsson/AdventOfCode/PassportValidator.cs
@@ -6,7 +6,10 @@ namespace TestAdventOfCode
 {
     public class PassportValidator
     {
+        private const string FilePathPassportRulesRegex = "../../../../AdventOfCode/InputData/PassportRulesRegex.txt";
         private static List<string> passportValidKeys = new List<string> { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid" };
+        //private static Dictionary<string, string> passportRules = new Dictionary<string, string> {
+            //{ "byr", "" }, {"iyr" }, {"eyr" }, {"hgt" }, {"hcl" }, {"ecl" }, {"pid" }, {"cid" } };
         public static string[] GetInputData(string filePath)
         {
             string[] rawPassports = InputDataHandler.ReadFileAsArray(filePath, "\r\n\r\n");
@@ -55,6 +58,76 @@ namespace TestAdventOfCode
             }
 
             return noOfValidPasswords;
+        }
+
+        public static int CountValidPassportsStrict(string[] rawPassports)
+        {
+            int noOfValidPasswords = 0;
+            foreach (string rawPassport in rawPassports)
+            {
+                Dictionary<string, string> parsedPassport = PassportValidator.ParseRawPassport(rawPassport);
+                if (PassportValidator.ValidatePassportStrict(parsedPassport))
+                {
+                    noOfValidPasswords++;
+                }
+            }
+
+            return noOfValidPasswords;
+        }
+
+        public static bool ValidatePassportStrict(Dictionary<string, string> parsedPassport)
+        {
+            bool passportIsValid = true;
+
+            // First test with the simple Validator
+            if (!PassportValidator.ValidatePassport(parsedPassport)) passportIsValid = false;
+
+            // Second, invalidate by Regex
+            else if (!PassportValidator.ValidatePassportRegex(parsedPassport)) passportIsValid = false;
+
+            return passportIsValid;
+        }
+
+        public static bool ValidatePassportRegex(Dictionary<string, string> parsedPassport)
+        {
+            bool passportIsValid = true;
+
+            // Obtain the passport rules
+            // TODO: This sghould only be new once. Maybe make into an instance variable?
+            string[] rawPassportTemplateRegex = InputDataHandler.ReadFileAsArray(FilePathPassportRulesRegex, "");
+            Dictionary<string, string> passportTemplateRegex = PassportValidator.ParseRawPassport(rawPassportTemplateRegex[0]);
+
+            // Compare value for a kay with the allowed Regex expression
+            // Loop over all template keys in case of an incomplete passport being supplied as argument
+            foreach (KeyValuePair<string, string> currentFieldTemplate in passportTemplateRegex)
+            {
+                // Don't care about cid, skip to checking next field
+                // TODO: Refactor to create all passport keys when reading the passport
+                if (currentFieldTemplate.Key == "cid")
+                {
+                    continue;
+                }
+
+                string value = string.Empty;
+                if (parsedPassport.TryGetValue(currentFieldTemplate.Key, out value))
+                {
+                    if (!Regex.IsMatch(value, currentFieldTemplate.Value))
+                    {
+                        passportIsValid = false;
+                        Console.WriteLine("Passport invalidated by: " + currentFieldTemplate.Key + ":" + value);
+                    }
+                }
+                else
+                {
+                    // If a passport doesn't include the template key it is atomatically invalid
+                    // TODO: Handle cid
+                    // TODO: Refactor the simple invalidator to use this method?
+                    passportIsValid = false;
+                }
+            }
+
+
+            return passportIsValid;
         }
     }
 }
--- ViktorNilsson/TestAdventOfCode/TestPassportValidator.cs
+++ ViktorNilsson/TestAdventOfCode/TestPassportValidator.cs
@@ -6,8 +6,9 @@ namespace TestAdventOfCode
 {
     public class TestPassportValidator
     {
-        private const string PuzzleFilePath = "../../../../AdventOfCode/InputData/DayFourInput.txt";
-        private const string TestDataFilePath = "../../../TestData/DayFourTestData.txt";
+        private const string FilePathInputData = "../../../../AdventOfCode/InputData/DayFourInput.txt";
+        private const string FilePathTestData = "../../../TestData/DayFourTestData.txt";
+        private const string FilePathTestDataStrict = "../../../TestData/DayFourTestDataStrict.txt";
 
         [SetUp]
         public void Setup()
@@ -17,14 +18,14 @@ namespace TestAdventOfCode
         [Test]
         public void TestGetInputData()
         {
-            string[] rawPassports = PassportValidator.GetInputData(TestDataFilePath);
+            string[] rawPassports = PassportValidator.GetInputData(FilePathTestData);
             Assert.AreEqual(4, rawPassports.GetLength(0));
         }
 
         [Test]
         public void TestParsePassport()
         {
-            string[] rawPassports = PassportValidator.GetInputData(TestDataFilePath);
+            string[] rawPassports = PassportValidator.GetInputData(FilePathTestData);
             Dictionary<string, string> parsedPassport = PassportValidator.ParseRawPassport(rawPassports[0]);
             Assert.AreEqual(8, parsedPassport.Count);
             Assert.IsTrue(parsedPassport.ContainsKey("ecl"));
@@ -37,7 +38,7 @@ namespace TestAdventOfCode
         [TestCase(3, false, TestName = "Valid Passport missing multiple fields")]
         public void TestValidatePassport(int iTestDataPassport, bool expectedValid)
         {
-            string[] rawPassports = PassportValidator.GetInputData(TestDataFilePath);
+            string[] rawPassports = PassportValidator.GetInputData(FilePathTestData);
             Dictionary<string, string> parsedPassport = PassportValidator.ParseRawPassport(rawPassports[iTestDataPassport]);
             bool passportValid = PassportValidator.ValidatePassport(parsedPassport);
             Assert.AreEqual(expectedValid, passportValid);
@@ -46,7 +47,7 @@ namespace TestAdventOfCode
         [Test]
         public void TestCountValidPassports()
         {
-            string[] rawPassports = PassportValidator.GetInputData(TestDataFilePath);
+            string[] rawPassports = PassportValidator.GetInputData(FilePathTestData);
             int nuOfValidPassports = PassportValidator.CountValidPassports(rawPassports);
             Assert.AreEqual(2, nuOfValidPassports);
         }
@@ -62,10 +63,50 @@ namespace TestAdventOfCode
         [Test]
         public void FindAnserDayFourPuzzleOne()
         {
-            string[] rawPassports = PassportValidator.GetInputData(PuzzleFilePath);
+            string[] rawPassports = PassportValidator.GetInputData(FilePathInputData);
             int nuOfValidPassports = PassportValidator.CountValidPassports(rawPassports);
             Console.WriteLine("Number of valid passports: " + nuOfValidPassports);
             Assert.AreEqual(228, nuOfValidPassports);
+        }
+
+        [Test]
+        public void TestCountValidPassportsStrictValid()
+        {
+            string[] rawPassports = PassportValidator.GetInputData(FilePathTestDataStrict);
+            int nuOfValidPassports = PassportValidator.CountValidPassportsStrict(rawPassports);
+            Assert.AreEqual(5, nuOfValidPassports);
+        }
+
+        [TestCase(0, true)]
+        [TestCase(1, true)]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true, TestName = "Invalid but OK to slip because of cid")]
+        [TestCase(6, false)]
+        [TestCase(7, false)]
+        public void TestValidatePassportStrict(int iTestDataPassport, bool expectedValid)
+        {
+            string[] rawPassports = PassportValidator.GetInputData(FilePathTestDataStrict);
+            Dictionary<string, string> parsedPassport = PassportValidator.ParseRawPassport(rawPassports[iTestDataPassport]);
+            bool passportValid = PassportValidator.ValidatePassportStrict(parsedPassport);
+            Assert.AreEqual(expectedValid, passportValid);
+        }
+
+        [TestCase(0, true)]
+        [TestCase(1, true)]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true, TestName = "Invalid accordingt Regex but OK to slip because of cid")]
+        [TestCase(6, false)]
+        [TestCase(7, false)]
+        public void TestValidatePassportRegex(int iTestDataPassport, bool expectedValid)
+        {
+            string[] rawPassports = PassportValidator.GetInputData(FilePathTestDataStrict);
+            Dictionary<string, string> parsedPassport = PassportValidator.ParseRawPassport(rawPassports[iTestDataPassport]);
+            bool passportValid = PassportValidator.ValidatePassportRegex(parsedPassport);
+            Assert.AreEqual(expectedValid, passportValid);
         }
     }
 }
