223b471356ad8fcb13df7eb2a8b66a753b3566dd
Askungn <Askungn@Terra>
2020-12-06 12:44:56 +0100

    day 6 cleanup code

--- Marcus/day6.py
+++ Marcus/day6.py
@@ -8,16 +8,9 @@ print(sum([len(set(g)-set("\n")) for g in data.split("\n\n")]))
 
 # part two
 
-new_answers = 0
-
-
 def get_common_yeses(group):
     sets = list(map(set, group.split()))
     return len(set.intersection(*sets))
 
 
 print(sum([get_common_yeses(group) for group in data.split("\n\n")]))
-
-
-
-
