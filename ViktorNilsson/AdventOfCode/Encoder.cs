9dd21cac35529093e71bc5189ea8c1c00a61431f
Viktor Nilsson <viktor_741@hotmail.com>
2020-12-10 20:35:48 +0100

    Day 6 puzzle 1 solved, puzzle 2 in the making

---
+++ ViktorNilsson/AdventOfCode/Encoder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace TestAdventOfCode
+{
+    public class Encoder
+    {
+        public static List<long> GetInputData(string filePath)
+        {
+            var inputData = new List<long>();
+            string[] inputDataRaw = InputDataHandler.ReadFileAsArray(filePath);
+
+            foreach (var s in inputDataRaw)
+            {
+                inputData.Add(long.Parse(s));
+            }
+
+            return inputData;
+        }
+
+        public static (long num, List<long> set) FindWeakness(List<long> data, int preambleLength)
+        {
+            long weaknessNumber = 0;
+            long currentNumber;
+            int i;
+            for (i = preambleLength; i < data.Count; i++)
+            {
+                currentNumber = data[i];
+                List<long> validNumbers = FindSums(data.GetRange(i - preambleLength, preambleLength));
+                if (!validNumbers.Contains(currentNumber))
+                {
+                    weaknessNumber = currentNumber;
+                    break;
+                }
+            }
+
+            return (weaknessNumber, data.GetRange(0, i));
+        }
+
+        public static List<long> FindSums(List<long> numbers)
+        {
+            var sums = new List<long>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = numbers.Count - 1; j > i ; j--)
+                {
+                    sums.Add(numbers[i] + numbers[j]);
+                }
+            }
+
+            sums.Sort();
+            return sums;
+        }
+
+        public static List<long> FindSumSet(List<long> data, long sumToFind)
+        {
+            var sumSet = new List<long>();
+            int i, j;
+            for (i = 1; i < data.Count; i++)
+            {
+                // TODO: Need to find sum of ALL values in the range, not just 2
+                var ammountToSum = data.Count - i;
+                var sums = FindSums(data.GetRange(0, i));
+
+                // Check if the end of the sum set has been found
+                if (sums.Contains(sumToFind))
+                {
+                    break;
+                }
+            }
+
+            for (j = 0; j < data.Count; j++)
+            {
+                var sums = FindSums(data.GetRange(j, i-j));
+                // When the sum to find is no longer present the lower index has been found
+                if (!sums.Contains(sumToFind))
+                {
+                    break;
+                }
+            }
+            int startIdx = j - 1;
+            sumSet = data.GetRange(startIdx, i - startIdx);
+
+            return sumSet;
+        }
+    }
+}
---
+++ ViktorNilsson/TestAdventOfCode/TestEncoder.cs
@@ -0,0 +1,78 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace TestAdventOfCode
+{
+    public class TestEncoder
+    {
+        private const string Day = "Nine";
+        private const string FilePathInputData = "../../../../AdventOfCode/InputData/Day" + Day + "Input.txt";
+        private const string FilePathInputData2 = "../../../../AdventOfCode/InputData/Day" + Day + "Input2.txt";
+        private const string FilePathTestData = "../../../TestData/Day" + Day + "TestData.txt";
+        private List<long> testData;
+        private List<long> inputData;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.testData = Encoder.GetInputData(FilePathTestData);
+            this.inputData = Encoder.GetInputData(FilePathInputData);
+        }
+
+        [Test]
+        public void TestGetInputData()
+        {
+            var localTestData = this.testData;
+            Assert.AreEqual(20, localTestData.Count);
+            Assert.AreEqual(35, localTestData[0]);
+        }
+
+        [Test]
+        public void TestFindWeakness()
+        {
+            int preambleLength = 5;
+            var weakness = Encoder.FindWeakness(this.testData, preambleLength);
+            Assert.AreEqual(127, weakness.num);
+        }
+
+        [Test]
+        public void TestFindSums()
+        {
+            int preambleLength = 5;
+            List<long> sum = Encoder.FindSums(new List<long> { 1, 2, 3 });
+            Assert.AreEqual(3, sum.Count);
+            Assert.AreEqual(new List<int> { 3, 4, 5 }, sum);
+        }
+
+        [Test]
+        public void FindAnswerDayNinePuzzleOne()
+        {
+            int preambleLength = 25;
+            var weakness = Encoder.FindWeakness(this.inputData, preambleLength);
+            Assert.Contains(weakness.num, this.inputData);
+            Assert.AreEqual(248131121, weakness.num);
+        }
+
+        [Test]
+        public void TestFindSumSet()
+        {
+            long sumToFind = 127;
+            List<long> set = Encoder.FindSumSet(this.testData, sumToFind);
+            Assert.AreEqual(4, set.Count);
+            Assert.AreEqual(new List<long> { 15, 25, 40, 47 }, set);
+        }
+
+        [Test]
+        public void FindAnswerDayNinePuzzleTwo()
+        {
+            var sumToFind = Encoder.FindWeakness(this.inputData, 25);
+            List<long> set = Encoder.FindSumSet(sumToFind.set, sumToFind.num);
+            Assert.AreEqual(4, set.Count);
+            Console.WriteLine("Smallest: " + set[0] + " Largest: " + set[set.Count]);
+            long weaknessSum = set[0] + set[set.Count];
+            Console.WriteLine("Weakness sum: " );
+
+        }
+    }
+}
