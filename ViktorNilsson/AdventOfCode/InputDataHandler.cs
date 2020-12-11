491def532ab72c8ba787b4a0313308638b093142
Viktor Nilsson <viktor_741@hotmail.com>
2020-12-11 21:14:21 +0100

    Solve on day 11 puzzle 1

--- ViktorNilsson/AdventOfCode/InputDataHandler.cs
+++ ViktorNilsson/AdventOfCode/InputDataHandler.cs
@@ -6,6 +6,8 @@ namespace TestAdventOfCode
 {
     public class InputDataHandler
     {
+        public int colIdxMax;
+        public int rowIdxMax;
 
         public static string[] ReadFileAsArray(string FilePath, string dataSplit = "\r\n")
         {
@@ -13,6 +15,28 @@ namespace TestAdventOfCode
             // string[] fileRows = Regex.Split(fileContent, @"\s+");
             string[] fileRows = fileContent.Split(new string[] { dataSplit }, StringSplitOptions.None);
             return fileRows;
+        }
+
+        public char[,] GetInputData(string filePath)
+        {
+            // Specify folder since it can be run from elsewhere, e.g. unittest
+            string[] stringMap = InputDataHandler.ReadFileAsArray(filePath);
+            int rows = stringMap.Length;
+            int columns = stringMap[0].Length;
+            char[,] map = new char[rows, columns];
+
+            this.colIdxMax = columns;
+            this.rowIdxMax = rows;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    map[row, col] = stringMap[row][col];
+                }
+            }
+
+            return map;
         }
     }
 }
