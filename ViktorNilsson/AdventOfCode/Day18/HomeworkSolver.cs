21d211157cf84726b41b1b7e058510edd330a0a9
Viktor Nilsson <viktor.nilsson@infotiv.se>
2020-12-22 20:42:27 +0100

    Day 18 started. Need to handle parenthesis

---
+++ ViktorNilsson/AdventOfCode/Day18/HomeworkSolver.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class HomeworkSolver : InputDataHandler
+    {
+        public static int Solve(string problem)
+        {
+            int answer = 0;
+
+            // Trim away whitespaces
+            var remainingProblem = Regex.Replace(problem, " ", string.Empty);
+            int firstArgument = int.Parse(remainingProblem[0].ToString());
+            remainingProblem = remainingProblem.Substring(1);
+
+            while (remainingProblem.Length > 0)
+            {
+                char operand = remainingProblem[0];
+                int secondArgument = int.Parse(remainingProblem[1].ToString());
+
+                // TODO: Handle parenthesis (first in the middle of the string then at the beginning, then multiple)
+                switch (operand)
+                {
+                    case '+':
+                        answer = firstArgument + secondArgument;
+                        break;
+                    case '*':
+                        answer = firstArgument * secondArgument;
+                        break;
+                }
+
+                firstArgument = answer;
+                remainingProblem = remainingProblem.Substring(2);
+            }
+
+            return answer;
+        }
+    }
+
+}
---
+++ ViktorNilsson/TestAdventOfCode/TestHomeworkSolver.cs
@@ -0,0 +1,63 @@
+namespace AdventOfCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+
+    public class TestHomeworkSolver
+    {
+        private const string DayPath = @"../../../../AdventOfCode/Day18/";
+        private const string FilePathInputData = DayPath + "InputData.txt";
+        private const string FilePathTestData = DayPath + "TestData.txt";
+        private string[] testData;
+        private string[] inputData;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.testData = HomeworkSolver.ReadFileAsArray(FilePathTestData);
+            this.inputData = HomeworkSolver.ReadFileAsArray(FilePathInputData);
+        }
+
+        [Test]
+        public void TestReadInputData()
+        {
+            Assert.AreEqual(4, this.testData.Length);
+        }
+
+        [TestCase("1+1", 2)]
+        [TestCase("2*2", 4)]
+        [TestCase("1+2*2", 6)]
+        [TestCase("1+1+2*2", 8)]
+        [TestCase("1+2*(2+1)", 9)]
+        public void TestSolveSimple(string problem, int expectedAnswer)
+        {
+            int answer = HomeworkSolver.Solve(problem);
+            Assert.AreEqual(expectedAnswer, answer);
+        }
+
+        [TestCase(0, 26)]
+        [TestCase(1, 437)]
+        [TestCase(2, 12240)]
+        [TestCase(3, 13622)]
+        public void TestSolve(int iProblem, int expectedAnswer)
+        {
+            int answer = HomeworkSolver.Solve(this.testData[iProblem]);
+            Assert.AreEqual(expectedAnswer, answer);
+        }
+
+        [Test]
+        public void FindAnserDaySixteenPuzzleOne()
+        {
+            Assert.Warn("Not solved");
+        }
+
+        [Test]
+        public void FindAnserDaySixteenPuzzleTwo()
+        {
+
+            Assert.Warn("Not solved");
+        }
+    }
+}
