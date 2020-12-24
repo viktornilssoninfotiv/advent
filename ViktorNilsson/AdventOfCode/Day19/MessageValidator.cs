6bd7a3f5a37fd5910ea2ddc448f2728c93f4342f
Viktor Nilsson <viktor.nilsson@infotiv.se>
2020-12-24 18:04:50 +0100

    Continue on Day 19

--- ViktorNilsson/AdventOfCode/Day19/MessageValidator.cs
+++ ViktorNilsson/AdventOfCode/Day19/MessageValidator.cs
@@ -7,6 +7,89 @@
 
     public class MessageValidator : InputDataHandler
     {
+        public static Dictionary<string, string> GetRules(string[] rawData)
+        {
+            var rules = new Dictionary<string, string>();
 
+            foreach (var row in rawData)
+            {
+                // Rules and messages are separated by empty row
+                if (row == string.Empty)
+                {
+                    break;
+                }
+
+                string[] kvp = Regex.Split(row, ": ");
+
+                // Remove quotations around letters
+                var rule = Regex.Replace(kvp[1], "\"", string.Empty);
+                rules[kvp[0]] = rule;
+            }
+
+            return rules;
+        }
+
+        public static List<string> GetMessages(string[] rawData)
+        {
+            var messages = new List<string>();
+
+            foreach (var row in rawData.Reverse())
+            {
+                // Rules and messages are separated by empty row
+                if (row == string.Empty)
+                {
+                    break;
+                }
+
+                messages.Add(row);
+            }
+
+            messages.Reverse();
+
+            return messages;
+        }
+
+        public static bool Validate(string message, Dictionary<string, string> rules)
+        {
+            bool valid = false;
+            List<string> validMessages = GetValidMessages(rules);
+            valid = validMessages.Contains(message);
+
+            return valid;
+        }
+
+        public static List<string> GetValidMessages(Dictionary<string, string> rules)
+        {
+            var validMessages = new List<string>();
+            string allValidMessages = string.Empty;
+
+            string currentRule = rules["0"];
+
+            allValidMessages = UnfoldRule(rules, currentRule);
+
+            validMessages = allValidMessages.Split('|').ToList();
+
+            return validMessages;
+        }
+
+        private static string UnfoldRule(Dictionary<string, string> rules, string currentRule)
+        {
+            string allValidMessages = string.Empty;
+            foreach (var subRule in currentRule)
+            {
+                // Keep the OR as is
+                if (subRule == '|')
+                {
+                    allValidMessages += subRule;
+                }
+                else
+                {
+                    allValidMessages += rules[subRule.ToString()];
+                }
+            }
+            allValidMessages = UnfoldRule(rules, allValidMessages);
+
+            return allValidMessages;
+        }
     }
 }
--- ViktorNilsson/TestAdventOfCode/TestMessageValidator.cs
+++ ViktorNilsson/TestAdventOfCode/TestMessageValidator.cs
@@ -21,6 +21,48 @@ namespace AdventOfCode
         }
 
         [Test]
+        public void TestGetRules()
+        {
+            Dictionary<string, string> rules = MessageValidator.GetRules(this.testData);
+            Assert.AreEqual(6, rules.Keys.Count);
+            Assert.AreEqual("b", rules["5"]);
+        }
+
+        [Test]
+        public void TestGetMessages()
+        {
+            List<string> messages = MessageValidator.GetMessages(this.testData);
+            Assert.AreEqual(5, messages.Count);
+            Assert.AreEqual("aaaabbb", messages[4]);
+        }
+
+        [Test]
+        public void TestGetValidMessages()
+        {
+            Dictionary<string, string> rules = MessageValidator.GetRules(this.testData);
+            string validMessagePath = TestMessageValidator.DayPath + "TestValidMessages.txt";
+            List<string> validMessages = MessageValidator.ReadFileAsArray(validMessagePath).ToList();
+
+            List<string> messages = MessageValidator.GetValidMessages(rules);
+
+            Assert.AreEqual(validMessages.Count, messages.Count);
+            Assert.AreEqual(validMessages, messages);
+        }
+
+        [TestCase(0, true)]
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(3, false)]
+        [TestCase(4, false)]
+        public void TestValidate(int iMessage, bool expectedValid)
+        {
+            List<string> messages = MessageValidator.GetMessages(this.testData);
+            Dictionary<string, string> rules = MessageValidator.GetRules(this.testData);
+            bool valid = MessageValidator.Validate(messages[iMessage], rules);
+            Assert.AreEqual(expectedValid, valid);
+        }
+
+        [Test]
         public void FindAnserDayNineteenPuzzleOne()
         {
             Assert.Warn("Not solved");
