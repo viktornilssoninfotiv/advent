2fb094eb8c9de465d9cfa548b5dacb0a42a2ef90
Viktor Nilsson <viktor.nilsson@infotiv.se>
2021-12-03 20:43:08 +0100

    Solve Day 1 Puzzle 2

--- ViktorNilsson/2021/src/sonarsweep.py
+++ ViktorNilsson/2021/src/sonarsweep.py
@@ -11,3 +11,13 @@ class SonarSweep:
             if self.depth_report[i_depth] > self.depth_report[i_depth - 1]:
                 increases += 1
         return increases
+
+    def count_depth_increases_3sum(self):
+        increases = 0
+        for i_depth in range(4, len(self.depth_report) + 1):
+            # Obtain the sum of 3 elements
+            current_sum = sum(self.depth_report[i_depth-3:i_depth])
+            previous_sum = sum(self.depth_report[i_depth-4:i_depth-1])
+            if current_sum > previous_sum:
+                increases += 1
+        return increases
--- ViktorNilsson/2021/test/Day1/testsonarsweep.py
+++ ViktorNilsson/2021/test/Day1/testsonarsweep.py
@@ -3,7 +3,7 @@ from sonarsweep import SonarSweep
 
 
 class TestSonarSweep(unittest.TestCase):
-    def test_simple_depth_input(self):
+    def test_count_depth_increases(self):
         sonar_sweep = SonarSweep("test_data.txt")
         increases = sonar_sweep.count_depth_increases()
         self.assertEqual(7, increases)
@@ -14,6 +14,16 @@ class TestSonarSweep(unittest.TestCase):
         print("Number of increases for Day 1 Puzzle 1: " + str(increases))
         self.assertEqual(1559, increases)
 
+    def test_count_depth_increases_3sum(self):
+        sonar_sweep = SonarSweep("test_data.txt")
+        increases = sonar_sweep.count_depth_increases_3sum()
+        self.assertEqual(5, increases)
+
+    def test_solve_day1_puzzle2(self):
+        sonar_sweep = SonarSweep("input.txt")
+        increases = sonar_sweep.count_depth_increases_3sum()
+        print("Number of increases for Day 1 Puzzle 2: " + str(increases))
+        self.assertEqual(1600, increases)
 
 if __name__ == '__main__':
     unittest.main()
